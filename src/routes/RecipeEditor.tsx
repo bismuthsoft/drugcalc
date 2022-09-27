5120ab31b93bb42d799ab6facc19b5f7f73c5788
Sam Pagenkopf <ssaammp@gmail.com>
2022-09-27 00:08:39 -0500

    added ingredients list placeholder to recipeeditor

--- src/routes/RecipeEditor.tsx
+++ src/routes/RecipeEditor.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { Menu } from 'antd';
+import { AutoComplete, Select, InputNumber } from 'antd';
+import './RecipeEditor.css';
+
+const colors = [
+    '#4cf', '#f8c', '#fc6', "#8f8"
+];
 
 type Ingredient = {
     name: string,
@@ -11,22 +16,32 @@ type Ingredient = {
 const RecipeEditor: React.FC = () => {
     const {recipe, recipeList}: any = useLoaderData();
     const ingredientsList = recipe.ingredients.map(
-        ({name, quantity, unit}: Ingredient, i: number) => (
-            <Ingredient />
+        (ingredient: Ingredient, i: number) => (
+            <Ingredient key={i} {...{ingredient, recipeList, index: i}}/>
         )
     );
     return (
         <>
+            <div>Name: {recipe.name}</div>
             <div className="recipe-editor-ingredients">{ingredientsList}</div>
             <div>RecipeEditor</div>
-            <div>Name: {recipe.name}</div>
         </>
     );
 }
 
-const Ingredient = () => {
+const Ingredient = ({ingredient, recipeList, index}: any) => {
+    const { name, quantity, unit } = ingredient;
+    const options = recipeList.map((r: string) => ({value: r}));
     return (
-        <div>Ingredient</div>
+        <div className="ingredient">
+            <div className="ingredient__color" style={{background: colors[index]}}></div>
+            <AutoComplete className="ingredient__name" value={name} options={options}/>
+            <InputNumber className="ingredient__quantity"
+                         min={0} max={99999} defaultValue={quantity} />
+            <Select className="ingredient__unit" defaultValue={unit}>
+                <Select.Option value="mg">mg</Select.Option>
+            </Select>
+        </div>
     );
 };
 
