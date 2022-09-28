728d60e70d96c40706309bb177fae663ab1311ce
Sam Pagenkopf <ssaammp@gmail.com>
2022-09-28 00:11:09 -0500

    convert recipe editor to table

--- src/routes/RecipeEditor.tsx
+++ src/routes/RecipeEditor.tsx
@@ -1,52 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './RecipeEditor.css';
 import { useLoaderData } from 'react-router-dom';
-import { AutoComplete, InputNumber } from 'antd';
+import { Table, AutoComplete, InputNumber, Button } from 'antd';
+import { PlusSquareOutlined } from '@ant-design/icons';
 import UnitSelector from '../UnitSelector';
 
 const colors = [
     '#4cf', '#f8c', '#fc6', "#8f8"
 ];
 
+type Recipe = {
+    name: string,
+    ingredients: Ingredient[],
+    containers: Container[],
+}
+
 type Ingredient = {
     name: string,
+    id: number,
     quantity: number,
     unit: string,
 };
+
+type Container = {
+    name: string,
+    quantity: number,
+}
 
 type Props = {
     readOnly?: boolean,
 }
 
 const RecipeEditor: React.FC<Props> = ({readOnly}) => {
-    const {recipe, recipeList}: any = useLoaderData();
-    const ingredientsList = recipe.ingredients.map(
-        (ingredient: Ingredient, i: number) => (
-            <Ingredient key={i} {...{ingredient, recipeList, index: i}}/>
-        )
-    );
+    const {recipe: recipe_data, recipeList}: {recipe: Recipe, recipeList: any} = useLoaderData() as any;
+    const [ recipe, setRecipe ] = useState(recipe_data);
+
+    const allIngredients = recipeList.map(({name}: any) => ({value: name}));
+    const ingredientsColumns = [
+        {
+            key: 'color',
+            width: 1,
+            render: (_:any, __:any, index: number) =>
+                <div className="ingredient__color"
+                style={{background: colors[index]}}/>,
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: (name: string) => (
+                <AutoComplete className="ingredient__name"
+                              value={name}
+                              options={allIngredients} />
+            )
+        },
+        {
+            title: 'Quantity',
+            dataIndex: 'quantity',
+            key: 'quantity',
+            width: 1,
+            render: (quantity:number) =>
+                <InputNumber className="ingredient__quantity"
+                             min={0} max={99999} step={10}
+                             defaultValue={quantity} />
+        },
+        {
+
+            title: 'Unit',
+            dataIndex: 'unit',
+            key: 'unit',
+            width: 1,
+            render: (unit: string) =>
+                <UnitSelector unit={unit} />,
+        }
+    ]
     return (
         <>
             <div>Name: {recipe.name}</div>
-            <div className="recipe-editor-ingredients">{ingredientsList}</div>
+            <Table dataSource={recipe.ingredients}
+                   columns={ingredientsColumns}
+                   rowKey="name"
+                   footer={() =>
+                       <Button
+                           onClick={() => setRecipe({
+                               ...recipe,
+                               ingredients: [
+                                   ...recipe.ingredients,
+                                   {
+                                       name: 'Another One',
+                                       id: Math.random(),
+                                       quantity: 0,
+                                       unit: 'mg',
+                                   }
+                               ]
+                           })}
+                       ><PlusSquareOutlined /></Button>
+                   }
+            />
             <div>RecipeEditor</div>
         </>
     );
 }
-
-const Ingredient = ({ingredient, recipeList, index}: any) => {
-    const { name, quantity, unit } = ingredient;
-    const options = recipeList.map(({name}: any) => ({value: name}));
-    return (
-        <div className="ingredient">
-            <div className="ingredient__color" style={{background: colors[index]}}></div>
-            <AutoComplete className="ingredient__name" value={name} options={options}/>
-            <InputNumber className="ingredient__quantity"
-                         min={0} max={99999} defaultValue={quantity} />
-            <UnitSelector unit={unit} />
-        </div>
-    );
-};
 
 export async function loader() {
     const recipe = await fetch("/api/recipes/0.json");
